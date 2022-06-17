6948479518c77fc152a24d775d0ff8a45080bfed
Daniela Becerra Gonzalez <danybecerr@gmail.com>
2022-06-17 00:33:53 -0500

    remove quotes

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "azuredevops_project" "myproject" {
   name       = "FlowwerDev"
   description        = "Project to test the Flower plugin development"
   features {
-    "testplans" = "disabled"
-    "boards"    = "disabled"
+    testplans = "disabled"
+    boards    = "disabled"
   }
 }
