6c50a1bde8f16eb0d50582996e9143fe8a823daf
Daniela Becerra Gonzalez <danybecerr@gmail.com>
2022-06-17 00:31:07 -0500

    Add features to project

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
 resource "azuredevops_project" "myproject" {
   name       = "FlowwerDev"
   description        = "Project to test the Flower plugin development"
+  features {
+    "testplans" = "disabled"
+    "boards"    = "disabled"
+  }
 }
